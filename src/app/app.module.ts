dbc9a433aba20a2d787c3ae86606e70627083e43
Gus-Vilela <gustavo.carvil@protonmail.com>
2022-02-02 14:17:10 -0300

    Add json data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TesteComponent } from './teste/teste.component';
-import { Teste2Component } from './teste2/teste2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TesteComponent,
-    Teste2Component
   ],
   imports: [
     BrowserModule
